7da0560d87a1ad3b19a30d065dfa0d02aa9c9c5c
AsmaaBasher <asmaabasher08@gmail.com>
2023-02-21 08:27:08 -0800

    pp

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,5 @@
-#include <stdio.h>
-
-int main() {
-  printf("_putchar\n");
-  printf("I am learning C.");
-  return 0;
-};
+#include <unistd.h>
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
